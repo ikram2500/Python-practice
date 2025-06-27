16a94b2d94b2ac730528447e79842a448d3b14bc
Muhammad Ikram <ikram2500@gmail.com>
2025-06-27 15:42:57 +0500

    some folders deleted

--- Expert Python/Tec With Tim/06_ContextManagers.py
+++
@@ -1,7 +0,0 @@
-"""file = open("file.txt","w")
-try:
-    file.write("Hello")
-finally:
-    file.close()"""
-with open("file.txt","w") as file:
-    file.write("Hello")
--- Introduction/list.py
+++
@@ -1,5 +0,0 @@
-fruit =['apple','cherry', 'banana']
-n = [1,2,3,4,5,6,7,8,9]
-new_fruit = n[:1:8]
-print(n)
-print(new_fruit)
